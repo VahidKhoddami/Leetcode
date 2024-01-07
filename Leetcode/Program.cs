92a14197cd8c6b3f4c68a4cb06744f6797fb5c9c
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-01-07 09:42:42 -0330

    Single number

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,4 +1,7 @@
 ﻿// Candy problem: https://leetcode.com/problems/candy/
-var candy = new CandyProblem();
+//var candy = new CandyProblem();
+//Console.WriteLine(candy.Candy(new int[] { 3, 2, 1, 4, 5 }).ToString());
 
-Console.WriteLine(candy.Candy(new int[] { 3, 2, 1, 4, 5 }).ToString());
+//Single Nnumber problem: https://leetcode.com/problems/single-number/
+var singleNumber = new SingleNumberProblem();
+Console.WriteLine(singleNumber.SingleNumber(new int[] { 1, 2, 3, 4, 5, 1, 2, 3, 4 }).ToString());
---
+++ Leetcode/SingleNumberProblem.cs
@@ -0,0 +1,12 @@
+﻿// single-number problem: https://leetcode.com/problems/single-number/description/
+
+public class SingleNumberProblem
+{
+    public int SingleNumber(int[] nums)
+    {
+        var result = 0;
+        foreach (var num in nums)
+            result ^= num;
+        return result;
+    }
+}
