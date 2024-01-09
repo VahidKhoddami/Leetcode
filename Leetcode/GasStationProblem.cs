85ee441bcfb79e6f5f1381c9df1d079fbc6927b0
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-01-09 13:14:45 -0330

    Gas station problem

---
+++ Leetcode/GasStationProblem.cs
@@ -0,0 +1,36 @@
+﻿// Gas station problem: https://leetcode.com/problems/gas-station/
+/*
+Intuition
+A real-world analogy would be a road trip with multiple potential starting points (gas stations). If at any point you run out of fuel before reaching the next station, it's clear that starting from any previous station wouldn't have worked either, since they had even less fuel. So, you would try starting again from the next station.
+
+Approach
+Start from the first station and keep track of the remaining gas in the tank as well as the total gas used.
+At each station, add gas[i] to the tank and subtract cost[i] (the cost to move to the next station).
+If at any point the gas in the tank becomes negative, it means you cannot travel further from the current starting station.
+Record the next station as the new starting point. Reset the remaining gas to zero since you are starting afresh.
+
+Complexity
+Time complexity: O(n)
+Space complexity: O(1)
+ */
+public class GasStationProblem
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        var output= 0;
+        var l = gas.Length;
+        var remainingGas = 0;
+        var totalGasUsed = 0;
+        for (int i = 0; i < l; i++)
+        {
+            remainingGas += gas[i] - cost[i];
+            totalGasUsed += gas[i] - cost[i];
+            if (remainingGas < 0)
+            {
+                remainingGas = 0;
+                output = i+1;
+            }
+        }
+        return totalGasUsed >= 0 ? output : -1;
+    }
+}
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -3,5 +3,9 @@
 //Console.WriteLine(candy.Candy(new int[] { 3, 2, 1, 4, 5 }).ToString());
 
 //Single Nnumber problem: https://leetcode.com/problems/single-number/
-var singleNumber = new SingleNumberProblem();
-Console.WriteLine(singleNumber.SingleNumber(new int[] { 1, 2, 3, 4, 5, 1, 2, 3, 4 }).ToString());
+//var singleNumber = new SingleNumberProblem();
+//Console.WriteLine(singleNumber.SingleNumber(new int[] { 1, 2, 3, 4, 5, 1, 2, 3, 4 }).ToString());
+
+// Gas station problem: https://leetcode.com/problems/gas-station/
+var gasStation = new GasStationProblem();
+Console.WriteLine(gasStation.CanCompleteCircuit(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 1, 2 }).ToString());
