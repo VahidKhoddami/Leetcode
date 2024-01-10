15691b3dcdb2a53b38e37b7516877ac100b4e99c
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-01-09 23:05:52 -0330

    Binary Tree Right Side View Problem

---
+++ Leetcode/BinaryTreeRightSideViewProblem.cs
@@ -0,0 +1,65 @@
+﻿// Binary tree right side view: https://leetcode.com/problems/binary-tree-right-side-view/
+
+using System.Drawing;
+/*
+Intuition
+Breadth-First Search (BFS) Approach: 
+This algorithm uses a queue data structure to implement a breadth-first search (BFS) traversal of the tree, which is necessary to identify the rightmost element at each level.
+
+
+Approach
+The algorithm processes nodes level by level. The outer while loop continues as long as there are nodes in the queue. Inside this loop, the algorithm processes each level of the tree.
+To capture the right most element For each level of the tree, the algorithm iterates through all the nodes at that level ("for" loop with l=queue.Count). The rightmost element is identified as the last element in the current level (if (i == l - 1)). This is because in BFS, nodes are enqueued and dequeued in a left-to-right order.
+To traverse the tree level by level, inside the loop, child nodes of the current node (node.left and node.right) are enqueued if they are not null.
+
+Complexity
+Time complexity: O(n), where n is the number of nodes in the binary tree where n is the number of nodes in the binary tree because each node in the tree is visited exactly once
+Space complexity: O(n), size of the queue can grow up to the maximum number of nodes at any level in the given binary tree
+    */
+public class BinaryTreeRightSideViewProblem
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public IList<int> RightSideView(TreeNode root)
+    {
+        var output = new List<int>();
+        if (root == null)
+        {
+            return output;
+        }
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        while (queue.Count > 0)
+        {
+            var l = queue.Count;
+            for (int i = 0; i < l; i++)
+            {
+                var node = queue.Dequeue();
+                if (i == l - 1)
+                {
+                    output.Add(node.val);
+                }
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+        }
+        return output;
+    }
+}
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -7,5 +7,20 @@
 //Console.WriteLine(singleNumber.SingleNumber(new int[] { 1, 2, 3, 4, 5, 1, 2, 3, 4 }).ToString());
 
 // Gas station problem: https://leetcode.com/problems/gas-station/
-var gasStation = new GasStationProblem();
-Console.WriteLine(gasStation.CanCompleteCircuit(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 1, 2 }).ToString());
+//var gasStation = new GasStationProblem();
+//Console.WriteLine(gasStation.CanCompleteCircuit(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 1, 2 }).ToString());
+
+// Binary tree right side view: https://leetcode.com/problems/binary-tree-right-side-view/
+var binaryTreeRightSideView = new BinaryTreeRightSideViewProblem();
+var root = new BinaryTreeRightSideViewProblem.TreeNode(1);
+root.left = new BinaryTreeRightSideViewProblem.TreeNode(2);
+root.right = new BinaryTreeRightSideViewProblem.TreeNode(3);
+root.left.right = new BinaryTreeRightSideViewProblem.TreeNode(5);
+root.right.right = new BinaryTreeRightSideViewProblem.TreeNode(4);
+var output = binaryTreeRightSideView.RightSideView(root);
+foreach (var item in output)
+{
+    Console.WriteLine(item);
+}
+
+
