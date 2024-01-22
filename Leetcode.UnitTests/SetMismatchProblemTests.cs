6bafbc6b140911701661a9fa5a2ab73e76853941
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-01-22 15:23:16 -0330

    Set Mismatch problem

---
+++ Leetcode.UnitTests/SetMismatchProblemTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Leetcode.UnitTests
+{
+    public class SetMismatchProblemTests
+    {
+        [Test]
+        public void SetMismatchProblem_Test1()
+        {
+            var setMismatchProblem = new SetMismatchProblem();
+            Assert.That(setMismatchProblem.FindErrorNums(new int[] { 1, 2, 2, 4 }), Is.EqualTo(new int[] { 2, 3 }));
+        }
+    }
+
+    
+}
