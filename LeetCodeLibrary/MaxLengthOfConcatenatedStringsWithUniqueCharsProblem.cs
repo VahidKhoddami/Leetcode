e34d85eed7b390fd70cdf0f2550d8c900f73fae7
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-01-24 09:18:41 -0330

    Maximum Length of a Concatenated String With Unique Characters Problem

---
+++ LeetCodeLibrary/MaxLengthOfConcatenatedStringsWithUniqueCharsProblem.cs
@@ -0,0 +1,53 @@
+﻿// Maximum Length of a Concatenated String With Unique Characters Problem Url: https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters
+public class MaxLengthOfConcatenatedStringsWithUniqueCharsProblem
+{
+    public int MaxLength(IList<string> arr)
+    {
+      
+        var result = 0;
+        var dp = new List<int> { 0 };
+        foreach (var s in arr)
+        {
+            var a = 0;
+            var dup = false;
+            var bits = 0;
+            foreach (var c in s)
+            {
+                bits |= 1 << (c - 'a');
+                if (((a >> (c - 'a')) & 1) == 1)
+                {
+                    dup = true;
+                }
+                a |= 1 << (c - 'a');
+            }
+            if (dup)
+            {
+                continue;
+            }
+            for (var i = dp.Count - 1; i >= 0; --i)
+            {
+                if ((dp[i] & bits) > 0)
+                {
+                    continue;
+                }
+                dp.Add(dp[i] | bits);
+                result = Math.Max(result, BitCount(dp[i] | bits));
+            }
+        }
+        return result;
+
+    }
+
+    private int BitCount(int n)
+    {
+        var count = 0;
+        while (n > 0)
+        {
+            n &= n - 1;
+            ++count;
+        }
+        return count;
+    }
+    
+}
+
---
+++ Leetcode.UnitTests/MaxLengthOfConcatenatedStringsWithUniqueCharsProblemTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.UnitTests
+{
+    internal class MaxLengthOfConcatenatedStringsWithUniqueCharsProblemTests
+    {
+        [Test]
+        public void MaxLengthOfConcatenatedStringsWithUniqueCharsProblem_Test1()
+        {
+            var maxLengthOfConcatenatedStringsWithUniqueCharsProblem = new MaxLengthOfConcatenatedStringsWithUniqueCharsProblem();
+            Assert.That(maxLengthOfConcatenatedStringsWithUniqueCharsProblem.MaxLength(new List<string> { "un", "iq", "ue" }), Is.EqualTo(4));
+        } 
+        [Test]
+        public void MaxLengthOfConcatenatedStringsWithUniqueCharsProblem_Test2()
+        {
+            var maxLengthOfConcatenatedStringsWithUniqueCharsProblem = new MaxLengthOfConcatenatedStringsWithUniqueCharsProblem();
+            Assert.That(maxLengthOfConcatenatedStringsWithUniqueCharsProblem.MaxLength(new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p" }), Is.EqualTo(16));
+        }
+        
+        [Test]
+        public void MaxLengthOfConcatenatedStringsWithUniqueCharsProblem_Test3()
+        {
+            var maxLengthOfConcatenatedStringsWithUniqueCharsProblem = new MaxLengthOfConcatenatedStringsWithUniqueCharsProblem();
+            Assert.That(maxLengthOfConcatenatedStringsWithUniqueCharsProblem.MaxLength(new List<string> { "cha", "r", "act", "ers" }), Is.EqualTo(6));
+        } 
+        
+        [Test]
+        public void MaxLengthOfConcatenatedStringsWithUniqueCharsProblem_Test4()
+        {
+            var maxLengthOfConcatenatedStringsWithUniqueCharsProblem = new MaxLengthOfConcatenatedStringsWithUniqueCharsProblem();
+            Assert.That(maxLengthOfConcatenatedStringsWithUniqueCharsProblem.MaxLength(new List<string> { "abcdefghijklmnopqrstuvwxyz" }), Is.EqualTo(26));
+        }
+    }
+}
