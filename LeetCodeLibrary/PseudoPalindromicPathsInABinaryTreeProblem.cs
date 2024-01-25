0fbfb86232bc3ca6fb75182cf3ab68fea45eed4c
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-01-25 10:50:34 -0330

    Pseudo-Palindromic Paths in a Binary Tree Problem

---
+++ LeetCodeLibrary/PseudoPalindromicPathsInABinaryTreeProblem.cs
@@ -0,0 +1,60 @@
+﻿//Pseudo-Palindromic Paths in a Binary Tree Url:https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/
+/*
+
+ */
+
+
+using System.ComponentModel;
+
+public class PseudoPalindromicPathsInABinaryTreeProblem
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int PseudoPalindromicPaths(TreeNode root)
+    {
+        var palindromePaths = 0;
+        var nodes = new Stack<(TreeNode,HashSet<int>)>();
+
+        nodes.Push((root, new HashSet<int>()));
+        
+        while (nodes.Count > 0)
+        {
+            var (node,values) = nodes.Pop();
+
+            if (!values.Add(node.val))
+                values.Remove(node.val);
+
+            if (node.left == null && node.right == null)
+            {
+
+                if (values.Count <= 1)
+                    palindromePaths++;
+
+
+            }
+            if (node.left != null)
+            {
+                nodes.Push((node.left,new HashSet<int>(values)));
+            }
+            if (node.right != null)
+            {
+                nodes.Push((node.right, new HashSet<int>(values)));
+            }
+        }
+
+        return palindromePaths;
+    }
+
+ 
+}
+
---
+++ Leetcode.UnitTests/PseudoPalindromicPathsInABinaryTreeProblemTests.cs
@@ -0,0 +1,21 @@
+﻿
+using static PseudoPalindromicPathsInABinaryTreeProblem;
+
+public class PseudoPalindromicPathsInABinaryTreeProblemTests
+{
+    [Test]
+    public void PseudoPalindromicPathsInABinaryTreeProblem_Test1()
+    {
+        var pseudoPalindromicPathsInABinaryTreeProblem = new PseudoPalindromicPathsInABinaryTreeProblem();
+        Assert.That(pseudoPalindromicPathsInABinaryTreeProblem.PseudoPalindromicPaths(new TreeNode(2, new TreeNode(3, new TreeNode(3), new TreeNode(1)), new TreeNode(1, null, new TreeNode(1)))), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void PseudoPalindromicPathsInABinaryTreeProblem_Test2()
+    {
+        var pseudoPalindromicPathsInABinaryTreeProblem = new PseudoPalindromicPathsInABinaryTreeProblem();
+        //[2,1,1,1,3,null,null,null,null,null,1]
+        Assert.That(pseudoPalindromicPathsInABinaryTreeProblem.PseudoPalindromicPaths(new TreeNode(2, new TreeNode(1, new TreeNode(1), new TreeNode(3,null,new TreeNode(1))), new TreeNode(1))), Is.EqualTo(1));
+    }
+}
+
