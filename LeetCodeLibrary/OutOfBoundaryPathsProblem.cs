7fe4ce516ab4346547aded6649b9ab9a67b79576
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-01-26 14:25:07 -0330

    Out of boundary paths problem

---
+++ LeetCodeLibrary/OutOfBoundaryPathsProblem.cs
@@ -0,0 +1,48 @@
+﻿// Out of boundary paths problem: https://leetcode.com/problems/out-of-boundary-paths/
+/*
+ Intuition
+I used bottom-up dynamic programming to solve it.
+
+Approach
+Here is a great explanation:
+[https://www.youtube.com/watch?v=Bg5CLRqtNmk&ab_channel=NeetCodeIO]()
+
+Complexity
+Time complexity: O(m * n * maxMove)
+Space complexity: O(m * n)
+ */
+
+public class OutOfBoundaryPathsProblem
+{
+    public int FindPaths(int m, int n, int maxMove, int startRow, int startColumn)
+    {
+        
+        var (rows, cols) = (m, n);
+        var dp = new int[rows, cols];
+        var mod = 1000000007;
+
+        for (int moves = 1; moves <= maxMove; moves++)
+        {
+            var temp = new int[rows, cols];
+            for (int r = 0; r < rows; r++)
+                for (int c = 0; c < cols; c++)
+                {
+                    //Rows go up and down 
+                    long up = r == 0 ? 1 : dp[r - 1, c];
+                    long down = r == rows - 1 ? 1 : dp[r + 1, c];
+                    //Columns go left and right
+                    long left = c == 0 ? 1 : dp[r, c - 1];
+                    long right = c == cols - 1 ? 1 : dp[r, c + 1];
+
+                    //Sum of all possible moves
+                    temp[r, c] = (int)((up + down + left + right) % mod);
+                }
+
+            dp = temp;
+        }
+
+        //Return out of path moves starting from startRow, startColumn
+        return dp[startRow, startColumn];
+    }
+}
+
---
+++ Leetcode.UnitTests/OutOfBoundaryPathsProblemTests.cs
@@ -0,0 +1,21 @@
+﻿
+public class OutOfBoundaryPathsProblemTests
+{
+    [Test]
+
+    public void FindPathsTest_Test1()
+    {
+        var sut = new OutOfBoundaryPathsProblem();
+        var result = sut.FindPaths(2, 2, 2, 0, 0);
+        Assert.That(result, Is.EqualTo(6));
+    }
+
+    [Test]
+    public void FindPathsTest_Test2()
+    {
+        var sut = new OutOfBoundaryPathsProblem();
+        var result = sut.FindPaths(8, 50, 23, 5, 26);
+        Assert.That(result, Is.EqualTo(914783380));
+    }
+}
+
