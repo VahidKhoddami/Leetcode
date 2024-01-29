39e410c037267be38854e01307f3d3df5f5fa9ba
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-01-28 21:42:54 -0330

    K Inverse Pairs Array Problem

---
+++ LeetCodeLibrary/KInversePairsArrayProblem.cs
@@ -0,0 +1,29 @@
+﻿// K Inverse Pairs Array Problem Url: https://leetcode.com/problems/k-inverse-pairs-array
+//https://www.youtube.com/watch?v=dglwb30bUKI&ab_channel=NeetCodeIO
+public class KInversePairsArrayProblem
+{
+    public int KInversePairs(int n, int k)
+    {
+        var prev = new int[k + 1];
+        prev[0] = 1;
+        var mod = 1000000007;
+
+        for (int i = 1; i <= n; i++)
+        {
+            var cur = new int[k + 1];
+            long total = 0;
+            for (int j = 0; j <= k; j++)
+            {
+                if (j >= i)
+                    total = (total - prev[j - i] + mod) % mod;
+                total = (total + prev[j]) % mod;
+                cur[j] = (int)total;
+            }
+
+            prev = cur;
+        }
+
+        return prev[k];
+    }
+}
+
---
+++ Leetcode.UnitTests/KInversePairsArrayProblemTests.cs
@@ -0,0 +1,28 @@
+﻿
+public class KInversePairsArrayProblemTests
+{
+    [Test]
+    public void KInversePairsTest_Test1()
+    {
+        var sut = new KInversePairsArrayProblem();
+        var result = sut.KInversePairs(3, 0);
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void KInversePairsTest_Test2()
+    {
+        var sut = new KInversePairsArrayProblem();
+        var result = sut.KInversePairs(3, 1);
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void KInversePairsTest_Test3()
+    {
+        var sut = new KInversePairsArrayProblem();
+        var result = sut.KInversePairs(1000, 1000);
+        Assert.That(result, Is.EqualTo(663677020));
+    }
+}
+
