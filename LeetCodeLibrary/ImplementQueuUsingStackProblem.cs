0c87109c089320f0fbd8ccd883cc1d14c6b8e753
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-01-31 14:17:25 -0330

    Implement Queu Using Stack Problem

---
+++ LeetCodeLibrary/ImplementQueuUsingStackProblem.cs
@@ -0,0 +1,49 @@
+﻿public class ImplementQueuUsingStackProblem
+{
+    public class MyQueue
+    {
+        private readonly Stack<int> _main;
+        private readonly Stack<int> _aux;
+
+        public MyQueue()
+        {
+            _main = new Stack<int>();
+            _aux = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            _main.Push(x);
+        }
+
+        public int Pop()
+        {
+            CopyMainToAuxWhenAuxEmpty();
+
+            return _aux.Pop();
+        }
+
+        public int Peek()
+        {
+            CopyMainToAuxWhenAuxEmpty();
+
+            return _aux.Peek();
+        }
+
+        public bool Empty()
+        {
+            return (_aux.Count + _main.Count) == 0;
+        }
+
+        private void CopyMainToAuxWhenAuxEmpty()
+        {
+            if (_aux.Count > 0)
+                return;
+            
+            while (_main.Count > 0)
+                _aux.Push(_main.Pop());
+        }
+    }
+
+}
+
---
+++ Leetcode.UnitTests/ImplementQueusUsingStacksProblemTests.cs
@@ -0,0 +1,26 @@
+﻿public class ImplementQueusUsingStacksProblemTests
+{
+    [Test]
+    public void Test1()
+    {
+        var sut = new ImplementQueuUsingStackProblem.MyQueue();
+        sut.Push(1);
+        sut.Push(2);
+        Assert.That(sut.Peek(), Is.EqualTo(1));
+        Assert.That(sut.Pop(), Is.EqualTo(1));
+        Assert.That(sut.Empty(), Is.False);
+    }
+
+    [Test]
+    public void Test2()
+    {
+        var sut = new ImplementQueuUsingStackProblem.MyQueue();
+        sut.Push(1);
+        sut.Push(2);
+        sut.Push(3);
+        Assert.That(sut.Peek(), Is.EqualTo(1));
+        Assert.That(sut.Pop(), Is.EqualTo(1));
+        Assert.That(sut.Empty(), Is.False);
+    }
+}
+
