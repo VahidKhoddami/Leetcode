338da8ed5d1ebe31dea72c3b7ad9ebb39df529a1
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-02-01 11:24:18 -0330

    Divide Array Into Arrays With Max Difference Problem

---
+++ LeetCodeLibrary/DivideArrayIntoArraysWithMaxDifferenceProblem.cs
@@ -0,0 +1,33 @@
+﻿// Divie Array into Arrays with Max Difference Problem Url: https://leetcode.com/problems/divide-array-into-arrays-with-max-difference
+
+/*
+ The intuition is that if we divide the array into subarrays of size 3, 
+then the difference between the first and the last element of each subarray will be the maximum. 
+This is because the array is sorted and the difference between the first and the last element of the subarray will be the maximum possible difference between any two elements of the subarray. 
+If this difference is greater than k, then we cannot divide the array into subarrays of size 3. 
+Otherwise, we can divide the array into subarrays of size 3.
+
+Complexity Analysis:
+Time Complexity: O(nlogn), where n is the length of the input array.
+Space Complexity: O(n), where n is the length of the input array.
+ */
+public class DivideArrayIntoArraysWithMaxDifferenceProblem
+{
+    public int[][] DivideArray(int[] nums, int k)
+    {
+        var n = nums.Length;
+        Array.Sort(nums);
+        var answer = new int[n / 3][];
+
+        for (int i = 0; i < n - 2; i += 3)
+        {
+            if (nums[i + 2] - nums[i] > k)
+                return new int[0][];
+
+            answer[i / 3] = new int[] { nums[i], nums[i + 1], nums[i + 2] };
+        }
+
+        return answer;
+    }
+}
+
---
+++ Leetcode.UnitTests/DivideArrayIntoArraysWithMaxDifferenceProblemTests.cs
@@ -0,0 +1,20 @@
+﻿
+public class DivideArrayIntoArraysWithMaxDifferenceProblemTests
+{
+    [Test]
+    public void DivideArrayTest_Test1()
+    {
+        var sut = new DivideArrayIntoArraysWithMaxDifferenceProblem();
+        var result = sut.DivideArray(new int[] { 1, 3, 4, 8, 7, 9, 3, 5, 1 }, 2);
+        Assert.That(result, Is.EqualTo(new int[][] { new int[] { 1, 1, 3 }, new int[] { 3, 4, 5 }, new int[] { 7,8,9} }));
+    }
+
+    [Test]
+    public void DivideArrayTest_Test2()
+    {
+        var sut = new DivideArrayIntoArraysWithMaxDifferenceProblem();
+        var result = sut.DivideArray(new int[] { 1, 3, 3, 2, 7, 3 }, 3);
+        Assert.That(result, Is.EqualTo(new int[][] { }));
+    }
+}
+
