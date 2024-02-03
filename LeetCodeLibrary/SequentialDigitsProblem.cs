9aaae7be14e4231fde312b130494beb0d02558fd
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-02-03 17:59:11 -0330

    Sequential Digits Problem

---
+++ LeetCodeLibrary/SequentialDigitsProblem.cs
@@ -0,0 +1,23 @@
+﻿public class SequentialDigitsProblem
+{
+
+    public IList<int> SequentialDigits(int low, int high)
+    {
+        var result = new List<int>();
+        var lowLength = low.ToString().Length;
+        var highLength = high.ToString().Length;
+
+        for (var i = lowLength; i <= highLength; i++)
+        {
+            for (var j = 1; j <= 9 - i + 1; j++)
+            {
+                var number = int.Parse(string.Join("", Enumerable.Range(j, i)));
+                if (number >= low && number <= high)
+                    result.Add(number);
+            }
+        }
+
+        return result;
+    }
+}
+
---
+++ Leetcode.UnitTests/SequentialDigitsProblemTests.cs
@@ -0,0 +1,19 @@
+﻿public class SequentialDigitsProblemTests
+{
+    [Test]
+    public void SequentialDigitsTest1()
+    {
+        var problem = new SequentialDigitsProblem();
+        var result = problem.SequentialDigits(100, 300);
+        Assert.That(result, Is.EqualTo(new[] { 123, 234 }));
+    }
+
+    [Test]
+    public void SequentialDigitsTest2()
+    {
+        var problem = new SequentialDigitsProblem();
+        var result = problem.SequentialDigits(1000, 13000);
+        Assert.That(result, Is.EqualTo(new[] { 1234, 2345, 3456, 4567, 5678, 6789, 12345 }));
+    }
+}
+
