c69c6a3ddabec7ae6691d91dc2abeba7f742deb2
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-02-03 17:58:20 -0330

    Partition array for maximum sum problem

---
+++ LeetCodeLibrary/PartitionArrayForMaximumSumProblem.cs
@@ -0,0 +1,20 @@
+﻿public class PartitionArrayForMaximumSumProblem
+{
+    public int MaxSumAfterPartitioning(int[] arr, int k)
+    {
+        var n = arr.Length;
+        var dp = new int[n + 1];
+        for (var i = 1; i <= n; i++)
+        {
+            var max = 0;
+            for (var j = 1; j <= k && i - j >= 0; j++)
+            {
+                max = Math.Max(max, arr[i - j]);
+                dp[i] = Math.Max(dp[i], dp[i - j] + max * j);
+            }
+        }
+
+        return dp[n];
+    }
+}
+
---
+++ Leetcode.UnitTests/PartitionArrayForMaximumSumProblemTests.cs
@@ -0,0 +1,19 @@
+﻿public class PartitionArrayForMaximumSumProblemTests
+{
+    [Test]
+    public void MaxSumAfterPartitioningTest1()
+    {
+        var problem = new PartitionArrayForMaximumSumProblem();
+        var result = problem.MaxSumAfterPartitioning(new[] { 1, 15, 7, 9, 2, 5, 10 }, 3);
+        Assert.That(result,Is.EqualTo(84));
+    }
+
+    [Test]
+    public void MaxSumAfterPartitioningTest2()
+    {
+        var problem = new PartitionArrayForMaximumSumProblem();
+        var result = problem.MaxSumAfterPartitioning(new[] { 1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3 }, 4);
+        Assert.That(result, Is.EqualTo(83));
+    }
+}
+
