5bc5df2ec92c476e1c6457893820441d854094f6
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-02-12 12:55:38 -0330

    Majority Element (Easy) problem

---
+++ LeetCodeLibrary/MajorityElementEProblem.cs
@@ -0,0 +1,22 @@
+﻿// Mjority Element (Easy) Problem URL: https://leetcode.com/problems/majority-element/
+
+//Solution using Using the Boyer–Moore majority vote algorithm
+public class MajorityElementEProblem
+{
+    public int MajorityElement(int[] nums)
+    {
+        var majorityElement = 0;
+        var count = 0;
+
+        foreach (var num in nums)
+        {
+            if (count == 0)
+                majorityElement = num;
+
+            count += num == majorityElement ? 1 : -1;
+        }
+
+        return majorityElement;
+    }
+}
+
---
+++ Leetcode.UnitTests/MajorityElementEProblemTests.cs
@@ -0,0 +1,31 @@
+﻿public class MajorityElementEProblemTests
+{
+    [Test]
+    public void MajorityElement_WhenCalled_ReturnsMajorityElement()
+    {
+        //Arrange
+        var majorityElementEProblem = new MajorityElementEProblem();
+        var nums = new int[] { 3, 2, 3 };
+
+        //Act
+        var result = majorityElementEProblem.MajorityElement(nums);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void MajorityElement_WhenCalled_ReturnsMajorityElement2()
+    {
+        //Arrange
+        var majorityElementEProblem = new MajorityElementEProblem();
+        var nums = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+
+        //Act
+        var result = majorityElementEProblem.MajorityElement(nums);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(2));
+    }
+}
+
