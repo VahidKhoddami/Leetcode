162291cf841cf5572207e5cc28ed9a6901ca42c4
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-02-15 11:08:26 -0330

    Rearrange Array Elements by Sign Medium Problem

---
+++ LeetCodeLibrary/RearrangeArrayElementsbySignMProblem.cs
@@ -0,0 +1,42 @@
+﻿// Rearrange Array Elements by Sign (M) Problem URL: https://leetcode.com/problems/rearrange-array-elements-by-sign/
+/*
+Intuition
+We can also use queue to solve this problem.
+
+Approach
+We can initialize two queues one for positive and one for negative numbers.
+First we go through all the numbers and add each number to the corresponding positive and negative queues.
+Then for each number in the nums array we replace that number from the positive or negative queue.
+
+Complexity
+Time complexity:O(n)O(n)O(n)
+Space complexity:O(n)O(n)O(n) 
+ */
+public class RearrangeArrayElementsbySignMProblem
+{
+    public int[] RearrangeArray(int[] nums)
+    {
+        var pos = new Queue<int>();
+        var neg = new Queue<int>();
+        var addPos = true;
+
+        foreach (var num in nums)
+            if (num > 0)
+                pos.Enqueue(num);
+            else
+                neg.Enqueue(num);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (addPos)
+                nums[i] = pos.Dequeue();
+            else
+                nums[i] = neg.Dequeue();
+
+            addPos = !addPos;
+        }
+
+        return nums;
+    }
+}
+
---
+++ Leetcode.UnitTests/RearrangeArrayElementsbySignMProblemTests.cs
@@ -0,0 +1,31 @@
+﻿public class RearrangeArrayElementsbySignMProblemTests
+{
+    [Test]
+    public void RearrangeArray_WhenCalled_ReturnsRearrangedArray()
+    {
+        //Arrange
+        var rArray = new RearrangeArrayElementsbySignMProblem();
+        var nums = new int[] { 3, 1, -2, -5, 2, -4 };
+
+        //Act
+        var result = rArray.RearrangeArray(nums);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(new int[] { 3, -2, 1, -5, 2, -4 }));
+    }
+
+    [Test]
+    public void RearrangeArray_WhenCalled_ReturnsRearrangedArray2()
+    {
+        //Arrange
+        var rArray = new RearrangeArrayElementsbySignMProblem();
+        var nums = new int[] { -1, 1 };
+
+        //Act
+        var result = rArray.RearrangeArray(nums);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(new int[] { 1, -1 }));
+    }
+}
+
