1c3b2b0518068c7631b2ecdb54b53b5fcc882ce4
Vahid Khoddami <vahid.khoddami@gamil.com>
2024-02-23 10:38:53 -0330

    Cheapest Flights Within K Stops MProblem

---
+++ LeetCodeLibrary/CheapestFlightsWithinKStopsMProblem.cs
@@ -0,0 +1,26 @@
+﻿// Cheapest Flights Within K Stops (Medium) Url: https://leetcode.com/problems/cheapest-flights-within-k-stops/
+//Solve using Bellman-Ford algorithm
+public class CheapestFlightsWithinKStopsMProblem
+{
+    public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k)
+    {
+        var prices = new long[n];
+        for (int i = 0; i < n; i++)
+        {
+            prices[i] = int.MaxValue;
+        }
+        prices[src] = 0;
+        for (int i = 0; i <= k; i++)
+        {
+            var temp = new long[n];
+            Array.Copy(prices, temp, n);
+            foreach (var flight in flights)
+            {
+                temp[flight[1]] = Math.Min(temp[flight[1]], prices[flight[0]] + flight[2]);
+            }
+            prices = temp;
+        }
+        return prices[dst] == int.MaxValue ? -1 : (int)prices[dst];
+    }
+}
+
---
+++ Leetcode.UnitTests/CheapestFlightsWithinKStopsMProblemTests.cs
@@ -0,0 +1,49 @@
+﻿public class CheapestFlightsWithinKStopsMProblemTests
+{
+    [Test]
+    public void FindCheapestPrice_WhenCalled_ReturnsCheapestPrice1()
+    {
+        //Arrange
+        var cFlights = new CheapestFlightsWithinKStopsMProblem();
+        var n = 3;
+        var flights = new int[][]
+        {
+            new int[] { 0, 1, 100 },
+            new int[] { 1, 2, 100 },
+            new int[] { 0, 2, 500 },
+        };
+        var src = 0;
+        var dst = 2;
+        var k = 1;
+
+        //Act
+        var result = cFlights.FindCheapestPrice(n, flights, src, dst, k);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(200));
+    }
+
+    [Test]
+    public void FindCheapestPrice_WhenCalled_ReturnsCheapestPrice2()
+    {
+        //Arrange
+        var cFlights = new CheapestFlightsWithinKStopsMProblem();
+        var n = 3;
+        var flights = new int[][]
+        {
+            new int[] { 0, 1, 100 },
+            new int[] { 1, 2, 100 },
+            new int[] { 0, 2, 500 },
+        };
+        var src = 0;
+        var dst = 2;
+        var k = 0;
+
+        //Act
+        var result = cFlights.FindCheapestPrice(n, flights, src, dst, k);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(500));
+    }
+}
+
